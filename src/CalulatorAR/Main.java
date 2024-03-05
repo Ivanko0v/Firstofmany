0768e84a6622e755f53023e439ae659ac24c640a
Ivanko0v <161503203+Ivanko0v@users.noreply.github.com>
2024-03-05 16:11:19 +0300

    Update Main.java
    version 1.1

--- src/CalulatorAR/Main.java
+++ src/CalulatorAR/Main.java
@@ -1,29 +1,50 @@
 package CalulatorAR;
 
 
+import java.io.IOException;
 import java.util.Scanner;
 public class Main {
     public static String calc(String input) {
         return input;
     }
-        public static void main (String[]args){
-            Converter converter = new Converter();
-            String[] actions = {"+", "-", "/", "*"};
-            String[] regexActions = {"\\+", "-", "/", "\\*"};
-            Scanner scn = new Scanner(System.in);
-            System.out.println("Введите уравнение:");
-            String exp = scn.nextLine();
-            int actionIndex = -1;
-            for (int i = 0; i < actions.length; i++)
-                if (exp.contains(actions[i])) {
-                    actionIndex = i;
-                    break;
+
+    public static void main(String[] args) throws IOException {
+        Converter converter = new Converter();
+        String[] actions = {"+", "-", "/", "*"};
+        String[] regexActions = {"\\+", "-", "/", "\\*"};
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Введите уравнение:");
+        String exp = scn.nextLine();
+        int actionIndex = -1;
+                for (int i = 0; i < actions.length; i++)
+            if (exp.contains(actions[i])) {
+                actionIndex = i;
+                break;
+
+            }
+            try {
+                if (actionIndex == -1) {
+                    throw new IllegalArgumentException();
                 }
-            if (actionIndex == -1) {
-                System.out.println("Некоректное выражение:");
+            } catch (IllegalArgumentException s) {
+                System.out.println("Не верный знак операции");
                 return;
             }
+
             String[] data = exp.split(regexActions[actionIndex]);
+            try {
+            if(data.length !=2) {
+                throw new IOException();}
+            } catch(IOException e){
+                    System.out.println("Выражения должно быть два");
+                    return;
+                }
+            try {
+                if (converter.isRoman(data[0]) != converter.isRoman(data[1])) {
+                    throw new IllegalArgumentException();}
+                } catch(IllegalArgumentException v){
+                System.out.println("Числа должны быть в одном формате!");
+            }
             if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
                 int a, b;
                 boolean isRoman = converter.isRoman(data[0]);
@@ -34,14 +55,16 @@ public class Main {
                     a = Integer.parseInt(data[0]);
                     b = Integer.parseInt(data[1]);
                 }
-                if (a > 10) {
-                    System.out.println("Ошибка номер десять!");
+
+                try {
+                    if ((a > 10) || (b > 10)) {
+                        throw new IOException();
+                    }
+                } catch (IOException e) {
+                    System.out.println("Ошибка номер десять");
                     return;
                 }
-                if (b > 10) {
-                    System.out.println("Ошибка номер десять!");
-                    return;
-                }
+
                 int result = 0;
                 switch (actions[actionIndex]) {
                     case "+":
@@ -58,18 +81,22 @@ public class Main {
                         break;
                 }
                 if (isRoman) {
-                    if (result <= 0) {
-                        System.out.println("В Риме нет отрицательных чисел");
-                    } else {
-                        System.out.println(converter.intToRoman(result));
+                    try {
+                        if (result <= 0) {
+                            throw new NullPointerException();
+                        }
+                    } catch (NullPointerException e) {
+                        System.out.println("В Риме нет отрицалельных чисел!");
+                        return;
                     }
+
+                    System.out.println(converter.intToRoman(result));
+
                 } else {
                     System.out.println(result);
                 }
-            } else {
-                System.out.println("Числа должны быть в одном формате!");
             }
 
         }
 
-}
+    }
