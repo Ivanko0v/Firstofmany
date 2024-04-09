61022e587e61dd79a22c88262de45095fb9cf514
Ivanko0v <161503203+Ivanko0v@users.noreply.github.com>
2024-04-09 15:18:29 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,46 +1,45 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Calculator{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         Main result = new Main();
-        System.out.println("Введите выражение:");                   
-        String expression = input.nextLine();           
-        String answer = result.calc(expression);        
+        System.out.println("Введите выражение:");
+        String expression = input.nextLine();
+        String answer = result.calc(expression);
 
 
-        System.out.println("Ответ:\n" + answer);      
+        System.out.println("Ответ:\n" + answer);
     }
 }
 
 class Main{
-    public static String calc(String input){
-        boolean romanOrArab = false;                      
-        String exception = "throws Exception";            
-        Main romanExamination = new Main();               
-        Main arabToRoman = new Main();                    
-        int result = 0;                                   
+    public static String calc(String input) throws IOException, NumberFormatException{
+        boolean romanOrArab = false;
+        Main romanExamination = new Main();
+        Main arabToRoman = new Main();
+        int result = 0;
         String[] inputSplit = input.split(" ");
-        if (inputSplit.length != 3){
-            return exception;                            
+        if (inputSplit.length != 3){ throw new IOException("Больше двух чисел");
         }
         Integer firstNumber = 0;
         Integer secondNumber = 0;
         try {
             firstNumber = Integer.valueOf(inputSplit[0]);
             secondNumber = Integer.valueOf(inputSplit[2]);
-        } catch (NumberFormatException e) {                         
+        } catch (NumberFormatException e) {
             try {
                 firstNumber = romanExamination.romanToArab(inputSplit[0]);
                 secondNumber = romanExamination.romanToArab(inputSplit[2]);
                 romanOrArab = true;
-            } catch (NumberFormatException ex) {                     
-                return exception;
+            } catch (NumberFormatException ex) {
+                throw new NumberFormatException("Числа должны быть одного формата");
             }
         }
 
         if ((firstNumber < 1) || (firstNumber > 10) || (secondNumber < 1) || (secondNumber > 10)){
-            return exception;                                       
+            throw new IOException("Не больше 10");
         }
 
         String sign = inputSplit[1];
@@ -50,15 +49,15 @@ class Main{
             case "*" -> result = firstNumber * secondNumber;
             case "/" -> result = firstNumber / secondNumber;
             default -> {
-                return exception;                                    
+                throw new IOException("Не правильно введён знак выражения");
             }
         }
 
-        String output;                                               
+        String output;
 
         if (romanOrArab){
             if(result < 1){
-                return exception;
+                throw new IOException("В риме нет отрицательных чисел");
             } else {
                 output = arabToRoman.arabToRome(result);
             }
@@ -69,21 +68,21 @@ class Main{
         return output;
     }
 
-    Integer romanToArab(String romanInput){                            
+    Integer romanToArab(String romanInput){
         int result = 0;
         int[] arab = {10, 9, 5, 4, 1};
         String[] roman = {"X", "IX", "V", "IV", "I"};
         for (int i = 0; i < arab.length; i++ ) {
             while (romanInput.indexOf(roman[i]) == 0) {
                 result += arab[i];
-                romanInput = romanInput.substring(roman[i].length());  
+                romanInput = romanInput.substring(roman[i].length());
             }
         }
 
         return result;
     }
 
-    String arabToRome(int arabInput){                                   
+    String arabToRome(int arabInput){
         String result = "";
         int value = 0;
         int[] arab = {100, 90, 50, 40, 10, 9, 5, 4, 1};
